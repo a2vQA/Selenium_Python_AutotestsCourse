dd3120aa9dcc7bd2ed4f56325ccabbe7f3354c06
Владислав Артёменко <v.artyomenko@qsoft.ru>
2021-04-27 17:51:25 +0300

    Задание на локали 3_6_9

---
+++ 3_6_9/test_items.py
@@ -0,0 +1,9 @@
+import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+def test_basket_exists_on_page(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(30)
+    assert browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket'), 'Кнопка добавления в корзину отсутствует на странице'
