0c9d3a57b4c4f180ae19aed766b392c0b44d35e7
Vladislav <hellzzzr@gmail.com>
2021-06-06 20:47:12 +0300

    timer fix

--- 3_6_9/test_items.py
+++ 3_6_9/test_items.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 
 def test_basket_exists_on_page(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    time.sleep(30)
+    time.sleep(10)
     assert browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket'), 'Кнопка добавления в корзину отсутствует на странице'
